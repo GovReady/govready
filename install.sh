d5f5796a111616639e7c37c03a0884c84e9d9589
Greg Elin <greg@fotonotes.net>
2014-06-02 17:53:00 -0400

    Add epel.sh script to install script.

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ log_info(){
 
 # default install location for bins and man
 : ${MANDIR:=/usr/local/man/man1}
-: ${PREFIX:=/usr/local/bin}
+: ${PREFIX:=/usr/local/govready}
 
 # get info about env. for default settings
 BASH_DEFAULT=$(command -v bash || (log_error "bash command not available." && exit 1))
@@ -33,14 +33,24 @@ INSTALL="install"
 BUILD_DIR=$(mktemp -d -t 'govready_build.XXXXXXXXXX')
 
 # what scripts install/uninstall
-BASH_TARGET="govready"
+BASH_TARGET="epel.sh"
 
 install_bins(){
-    curl -Lksf "https://raw.githubusercontent.com/wilas/vbkick/${branch}/${BASH_TARGET}" -o "${BUILD_DIR}/${BASH_TARGET}.curl" ||\
+    TEMP_SRC="https://raw.githubusercontent.com/GovReady/govready/master/scripts/epel.sh"
+    # https://raw.githubusercontent.com/GovReady/govready/master/scripts/epel.sh
+    curl -Lksf "${TEMP_SRC}" -o "${BUILD_DIR}/${BASH_TARGET}" ||\
         (log_error "download govready bin failed." && return 1)
-    ${INSTALL} -m 0755 -d "${PREFIX}"
-    ${INSTALL} -m 0755 -p "${BUILD_DIR}/${BASH_TARGET}.tmp" "${PREFIX}/${BASH_TARGET}"
-    rm -rf "${BUILD_DIR}"
+    #${INSTALL} -m 0755 -d "${PREFIX}"
+    #${INSTALL} -m 0755 -p "${BUILD_DIR}/${BASH_TARGET}.tmp" "${PREFIX}/${BASH_TARGET}"
+    #rm -rf "${BUILD_DIR}"
 }
 
+
+# No error handling yet. Just testing.
+
+echo "Installing into ${BUILD_DIR}/${BASH_TARGET}...\n"
+install_bins
+log_info "Install succeeded."
+
+echo "Done\n"
 
