9c63fdba2608d37ef701c1af9bf7412abaf43afb
Rodney Cobb <rcobb@lllments.com>
2014-08-20 23:12:58 -0400

    Update epel.sh

--- scripts/epel.sh
+++ scripts/epel.sh
@@ -2,5 +2,4 @@
 
 # Install epel repos
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
